57bab72cb393adf267b5ca62ea60678109bd3d40
Christian <christian@anysrc.net>
2015-09-13 18:58:08 +0200

    Final switch to symfony/console

--- cmd.php
+++ cmd.php
@@ -3,19 +3,10 @@
 
 define("DIR_ABSOLUTEBASE", __DIR__.DIRECTORY_SEPARATOR);
 
-//--> Init script
-header("Content-Type: text/plain; charset=UTF-8", true);
-require __DIR__."/vendor/autoload.php";
-use \Symfony\Component\HttpFoundation\Request;
-use AnySrc\Shell\Stdout;
-use AnySrc\Shell\Args;
-use AnySrc\MarkdownBlog\PageCollection;
-use AnySrc\MyApplication;
+require __DIR__.'/vendor/autoload.php';
 use AnySrc\Shell\System;
-
-
-//--> Initialize application
-$app = new MyApplication();
+use AnySrc\Shell\Stdout;
+use AnySrc\MarkdownBlog\PageCollection;
 
 
 //--> Checks
@@ -30,11 +21,6 @@ if(!System::isLinux())
    Stdout::errl("Currently only Linux is supported.");
    exit(1);
 }
-
-
-//--> Parse arguments
-unset($argv[0]);
-$args = Args::fromArgv($argv);
 
 
 //--> TAN provider
@@ -63,178 +49,159 @@ $collection = new PageCollection($app);
 $collection->loadByRegexGlob(new AnySrc\RecursiveRegexGlob(__DIR__."/post", '/.+\.md$/'));
 
 
+//--> Load App
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface as II;
+use AnySrc\Shell\ConsoleOutput as OI;
+use \AnySrc\MarkdownBlog\Version;
+
+$app = new Application('anysrc mdblog', Version::getVersion());
+
+
 //--> TAN for login
-$app->get('newtan', function() use($tan, $cfg)
+$app->register('newtan')->setCode(function(II $ii, OI $oi) use($tan, $cfg)
 {
    $tstr = $tan->newLoginTan();
-   Stdout::writel("New Tan: [highlight]".$tstr."[/highlight]");
-   Stdout::writel("Login URL: [highlight]".$cfg->getPath('hostconfig/cmdhost', '/')."session/login#".$tstr."[/highlight]");
-   return "";
-});
+   $oi->writeln("New Tan: <ch>".$tstr."</ch>");
+   $oi->writeln("Login URL: <ch>".$cfg->getPath('hostconfig/cmdhost', '/')."session/login#".$tstr."</ch>");
+})
+->setDescription('Generate a new login TAN');
 
 
 //--> Clear all sessions
-$app->get('cleartokens', function() use($tan)
+$app->register('cleartokens')->setCode(function(II $ii, OI $oi) use($tan)
 {
-   Stdout::writel("Clear all TANs and open sessions...");
+   $oi->writeln('Clear all TANs and open sessions...');
    $tan->clear();
-   Stdout::writel("Done!");
-   return "";
-});
+   $oi->writeln("Done!");
+})
+->setDescription('Delete all TANs and login sessions');
 
 
 //--> List all sessions
-$app->get('sessions', function() use($tan)
+$app->register('sessions')->setCode(function(II $ii, OI $oi) use($tan)
 {
-   Stdout::writel("Display unused TANs and current sessions.");
-   Stdout::writel("Keys are stored as hash.");
-   Stdout::nl();
+   $oi->writeln("Display unused TANs and current sessions.");
+   $oi->writeln("Keys are stored as hash.");
+   $oi->writeln();
 
    $unusedtans = $tan->getTans();
-   Stdout::writel("[head]Unused TANs (hashed, not plaintext)[/head]");
+   $oi->titleln("Unused TANs (hashed, not plaintext)");
    if(is_array($unusedtans) && count($unusedtans)>0)
    {
       foreach($unusedtans as $unusedtan => $taninfo)
       {
-         Stdout::writel("[highlight]".$unusedtan."[/highlight]");
-         Stdout::writel("   Timeout: ".$taninfo['timeout']);
+         $oi->hlln($unusedtan);
+         $oi->writeln("   Timeout: ".$taninfo['timeout']);
       }
    }
    else
    {
-      Stdout::errl("No unused TANs found.");
+      $oi->errln("No unused TANs found.");
    }
 
-   Stdout::nl();
-   Stdout::writel("[head]Current sessions (hashed, not plaintext)[/head]");
+   $oi->writeln();
+   $oi->titleln("Current sessions (hashed, not plaintext)");
 
    $currentsessions = $tan->getSessions();
-   if(is_array($currentsessions) && count($currentsessions))
+   if(is_array($currentsessions) && count($currentsessions)>0)
    {
       foreach($currentsessions as $currentsession => $sessioninfo)
       {
-         Stdout::writel("[highlight]".$currentsession."[/highlight]");
-         Stdout::writel("   IP: ".$sessioninfo['ip']);
-         Stdout::writel("   Timeout: ".$sessioninfo['timeout']);
+         $oi->hlln($currentsession);
+         $oi->writeln("   IP: ".$sessioninfo['ip']);
+         $oi->writeln("   Timeout: ".$sessioninfo['timeout']);
       }
    }
    else
    {
-      Stdout::errl("No sessions found.");
+      $oi->errln("No sessions found.");
    }
-
-   return "";
-});
-
-
-//--> Find posts
-$app->get('find/{pattern}', function($pattern) use($collection)
-{
-   $properties = array("title", "name", "tags");
-   $result = $collection->findPages($properties, $pattern);
-
-   if($result->getCount()>0)
-   {
-      Stdout::writel("[head]Found ".$result->getCount()." page(s):[/head]");
-      Stdout::nl();
-
-      $result->sortAsc("name");
-      foreach($result as $page)
-      {
-         Stdout::pagenamel($page);
-      }
-   }
-   else
-   {
-      Stdout::errl("Nothing found.");
-   }
-
-   return "";
 })
-->assert('pattern', '.*');
+->setDescription('List all TANs and active sessions');
 
 
-//--> Latest posts
-$app->get('latest/{num}', function($num) use($collection)
+//--> List pages
+$app->register('pages')->setCode(function(II $ii, OI $oi) use($collection)
 {
-   $result = $collection->sortDesc('postdate')->getPagesByRange($num)->sortAsc('postdate');
+   $qry = $ii->getOption('search');
+   $noi = ((int)$ii->getOption('top'));
 
-   if($result->getCount()>0)
+   if($noi<=0)
    {
-      Stdout::writel("[head]List last ".$result->getCount()." page(s):[/head]");
-      Stdout::nl();
+      $noi=100;
+   }
 
-      foreach($result as $page)
-      {
-         Stdout::pagenamel($page);
-      }
+   $oi->errln("Limit output to ".$noi." page".($noi==1 ? "" : "s"));
+
+   if(!empty($qry))
+   {
+      $queryparser = new \AnySrc\SearchPropertyParser($qry);
+      $search = new \AnySrc\MarkdownBlog\PageSearch($collection);
+      $collection = $search->search($queryparser, 2, true);
+      $collection->sort(array(array('property'=>'searchscore', 'direction'=>'ASC')));
    }
    else
    {
-      Stdout::errl("Nothing found.");
+      $collection->sort(array(array('property'=>'postdate', 'direction'=>'DESC')));
    }
 
-   return "";
-})
-->assert('num', '^[0-9]+$')
-->value('num', 10);
+   $collection = $collection->getPagesByRange($noi, 0);
 
+   if(!empty($qry))
+   {
+      $collection->sort(array(array('property'=>'searchscore', 'direction'=>'DESC')));
+   }
+   else
+   {
+      $collection->sort(array(array('property'=>'postdate', 'direction'=>'ASC')));
+   }
 
-$app->get('latest', function() use($app)
-{
-   $subRequest = Request::create('/latest/10', 'GET');
-   return $app->handle($subRequest, \Symfony\Component\HttpKernel\HttpKernelInterface::SUB_REQUEST);
-});
+   $oi->titleln('Found '.$collection->getCount().' page'.($collection->getCount()==1 ? '' : 's'));
+   $oi->writeln();
 
-
-//--> List posts
-$app->get('list', function() use($collection)
-{
-   Stdout::writel("[head]Found ".$collection->getCount()." page(s):[/head]");
-   Stdout::nl();
-
-   $collection->sortAsc("name");
    foreach($collection as $page)
    {
-      Stdout::pagenamel($page);
+      $oi->pageln($page);
    }
 
-   Stdout::nl();
-   Stdout::writel("Flags: [error]-d-[/error]isabled, ".
-      "[error]-h-[/error]idden, [error]-p-[/error]rivate category");
-
-   return "";
-});
-
-
-//--> Search for page and edit first
-$app->get('writefind/{name}', function($name) use($app, $collection)
-{
-   $properties = array("hash", "name", "title", "tags");
-   $result = $collection->findPagesFirst($properties, $name);
-
-   if($result instanceof \AnySrc\MarkdownBlog\Page)
+   $oi->writeln();
+   if($collection->getFirstPage()->getIsPropExists('searchscore'))
    {
-      // Open editor
-      $subRequest = Request::create('/write/'.$result->getHash(), 'GET');
-      return $app->handle($subRequest, \Symfony\Component\HttpKernel\HttpKernelInterface::SUB_REQUEST);
+      $oi->title('Score: ');
+      $oi->writeln('search score, lower = better');
+   }
+
+   $oi->writeln("Flags: <ce>-d-</ce>isabled, <ce>-h-</ce>idden, <ce>-p-</ce>rivate category");
+})
+->setDescription('List or find pages')
+->addOption('top', 't', InputOption::VALUE_OPTIONAL, 'Number of listed pages')
+->addOption('search', 's', InputOption::VALUE_OPTIONAL, 'Search pattern');
+
+
+//--> Write / Writefind
+$app->register('write')->setCode(function(II $ii, OI $oi) use($collection)
+{
+   $name = $ii->getArgument('name');
+   $ff = $ii->getOption('findfirst');
+   $page = null;
+
+   if($ff)
+   {
+      $queryparser = new \AnySrc\SearchPropertyParser($name);
+      $search = new \AnySrc\MarkdownBlog\PageSearch($collection);
+      $collection = $search->search($queryparser, 5, true);
+      $collection->sort(array(array('property'=>'searchscore', 'direction'=>'ASC')));
+      $page = $collection->getFirstPage();
    }
    else
    {
-      Stdout::errl("Page not found");
+      $page = $collection->getPageAuto($name);
    }
 
-   return "";
-})
-->assert('name', '.*');
-
-
-//--> Edit post
-$app->get('write/{name}', function($name) use($collection)
-{
-   $page = $collection->getPageAuto($name);
-
-   if($page===null && strpos($name, "::")!==0)
+   if($page===null && $ff===false && strpos($name, "::")!==0)
    {
       $choice = Stdout::readprompt("Page not found. Create? (y/n)");
       if($choice=="y")
@@ -243,239 +210,165 @@ $app->get('write/{name}', function($name) use($collection)
       }
       else
       {
-         Stdout::errl("Aborted.");
-         exit(1);
+         $oi->errln('Aborted.');
+         return;
       }
    }
    elseif($page===null)
    {
-      Stdout::errl("Page not found.");
-      exit(1);
+      $oi->errln('Page not found.');
+      return;
    }
 
-   Stdout::write("Edit ");
-   Stdout::pagenamel($page);
+   $oi->write('Edit ');
+   $oi->pageln($page);
 
    $editor = System::getFirstAvailableCommand(array(System::getEnv("EDITOR"), "nano", "vim", "vi"));
    if($editor===null)
    {
-      Stdout::errl("No compatible editor found.");
-      Stdout::errl("Please set \$EDITOR environment variable.");
-      exit(1);
+      $oi->errln("No compatible editor found.");
+      $oi->errln("Please set \$EDITOR environment variable.");
+      return;
    }
 
-   Stdout::writel("Using [highlight]".$editor."[/highlight] as editor");
-   Stdout::writel("Launch...");
+   $oi->writeln("Using <ch>".$editor."</ch> as editor");
+   $oi->writeln("Launch...");
    sleep(1);
 
    System::ttyenvexec($editor, $page->getAbsoluteFilename());
 
-   Stdout::write("Done editing ");
-   Stdout::pagenamel($page);
-   return "";
+   $oi->writeln("Done editing ");
+   $oi->pageln($page);
 })
-->assert('name', '.*');
+->setDescription('Edit or create a page')
+->addOption('findfirst', 'f', InputOption::VALUE_NONE, 'If set, find the best matching post and start editing')
+->addArgument('name', InputArgument::REQUIRED, 'Filename, searchpattern, hashcode');
 
 
-$app->get('delete/{name}', function($name) use($collection)
+//--> Delete page
+$app->register('delete')->setCode(function(II $ii, OI $oi) use($collection)
 {
+   $name = $ii->getArgument('name');
    $page = $collection->getPageAuto($name);
+
    if($page instanceof \AnySrc\MarkdownBlog\Page)
    {
-      Stdout::writel("Page found.");
-      Stdout::pagenamel($page);
-      Stdout::nl();
+      $oi->write("Page found: ");
+      $oi->pageln($page);
+      $oi->writeln();
 
       $choice = Stdout::readprompt("Really delete? (y/n)");
       if($choice=="y")
       {
          $page->delete();
-         Stdout::writel("Page deleted.");
+         $oi->writeln("Page deleted.");
       }
       else
       {
-         Stdout::writel("Aborted.");
+         $oi->errln("Aborted.");
       }
    }
    else
    {
-      Stdout::errl("No page found.");
+      $oi->errln("No page found.");
    }
-
-   return "";
 })
-->assert('name', '.*');
+->setDescription('Delete a page')
+->addArgument('name', InputArgument::REQUIRED, 'Filename or hashcode');
 
 
-$app->get('folders', function() use($app, $collection)
+//--> Folders
+$app->register('folders')->setCode(function(II $ii, OI $oi) use($collection)
 {
    $folders = new AnySrc\RecursiveRegexGlob($collection->getBaseFolder(), null, "/.*/", false, true);
    $folders->sortAsc();
 
-   Stdout::writel("[head]".$folders->count()." folders found.[/head]");
-   Stdout::nl();
+   $oi->titleln($folders->count()." folder".($folders->count()==1 ? "" : "s")." found.");
+   $oi->writeln();
 
    foreach($folders as $folder)
    {
       $mdfolder = substr($folder, strlen($collection->getBaseFolder()));
-      Stdout::write("[file]~/".$mdfolder."[/file] ");
-      Stdout::write($collection->getFolderPages($mdfolder)->getCount()." pages inside");
+      $oi->file("~/".$mdfolder." ");
+      $oi->write($collection->getFolderPages($mdfolder)->getCount()." pages inside");
       if(is_file($folder.DIRECTORY_SEPARATOR."folder.yml"))
       {
-         Stdout::writel(", folder.yml");
+         $oi->writeln(", folder.yml");
       }
       else
       {
-         Stdout::nl();
+         $oi->writeln();
       }
    }
-   return "";
-});
-
-
-$app->get('set/{mode}/{file}', function($mode, $file) use($collection)
-{
-   $page = $collection->getPageAuto($file);
-   if(is_null($page))
-   {
-      Stdout::errl("Page not found.");
-      exit(1);
-   }
-
-   $change = null;
-   if($mode=="enable" || $mode=="disable")
-   {
-      $page->setIsDisabled(($mode=="disable"));
-      $change = "disabled: [highlight]".($mode=="disable" ? "true" : "false")."[/highlight]";
-   }
-   elseif($mode=="show" || $mode=="hide")
-   {
-      $page->setIsVisible(($mode=="show"));
-      $change = "visible: [highlight]".($mode=="show" ? "true" : "false")."[/highlight]";
-   }
-   else
-   {
-      throw new \Exception("Invalid mode");
-   }
-
-   Stdout::pagenamel($page);
-   Stdout::writel($change);
-
-   $page->write();
-   return "";
 })
-->assert('file', '.*')
-->assert('mode', '[^\s]+');
+->setDescription('List all folders');
 
 
-$app->get('upload', function() use ($cfg)
+//--> State changes
+$app->register('changestate')->setCode(function(II $ii, OI $oi) use($collection)
 {
-   Stdout::writel("Upload URL: [highlight]".$cfg->getPath('hostconfig/cmdhost', '/')."session/upload[/highlight]");
-   return "";
-});
+   $pages = $ii->getArgument('pages');
+   $enabled = ($ii->getOption('enable') ? true : ($ii->getOption('disable') ? false : null));
+   $visible = ($ii->getOption('show') ? true : ($ii->getOption('hide') ? false : null));
 
-
-$app->get('version', function()
-{
-   Stdout::writel("anysrc mdblog version: [highlight]".AnySrc\MarkdownBlog\Version::getVersion()."[/highlight]");
-   return "";
-});
-
-
-//--> Unknown action
-$app->error(function(\Exception $e, $code) use ($app)
-{
-   Stdout::nl();
-   if($code==404)
+   if($enabled!==null)
    {
-      Stdout::errl("Option not found. ".$e->getMessage());
-   }
-   else
-   {
-      Stdout::errl("ERROR: ".$e->getMessage());
+      $oi->writeln('Change state to <ch>'.($enabled===true ? 'enabled' : 'disabled').'</ch>');
    }
 
-   // Core help
-   $commands = array(
-       array(
-           "title" => "Login Session",
-           "commands" => array(
-               "newtan" => "Generate a new login TAN",
-               "cleartokens" => "Invalidate all login sessions and TANs",
-               "sessions" => "Display unused TANs and current sessions",
-           ),
-       ),
-       array(
-           "title" => "Find",
-           "commands" => array(
-               "list" => "List all posts",
-               "folders" => "List all folders",
-               "find <pattern>" => "Find posts by search pattern",
-               "latest 10" => "Display newest 10 pages",
-           ),
-       ),
-       array(
-           "title" => "Compose",
-           "commands" => array(
-               "write <::hash / some/file>" => "Create/Edit post by name or hash code",
-               "writefind <pattern>" => "Edit the first search result",
-               "delete <::hash / some/file>" => "Delete post by name or hash code",
-           ),
-       ),
-       array(
-           "title" => "Post Settings",
-           "commands" => array(
-               "set enable <::hash / some/file>" => "Enable post by name or hash code",
-               "set disable <::hash / some/file>" => "Disable post by name or hash code",
-               "set show <::hash / some/file>" => "Show post in list by name or hash code",
-               "set hide <::hash / some/file>" => "Hide post in list by name or hash code",
-           ),
-       ),
-       array(
-           "title" => "Other",
-           "commands" => array(
-               "upload" => "Display upload url",
-               "version" => "Print version code",
-           ),
-       ),
-   );
-
-   // Plugin help
-   if(isset($app['pluginmanager']))
+   if($visible!==null)
    {
-      $mgr = $app['pluginmanager'];
-      foreach($mgr->getPluginKeys() as $plugin)
+      $oi->writeln('Change visibility to <ch>'.($visible===true ? 'visible' : 'hidden').'</ch>');
+   }
+
+   if($enabled===null && $visible===null)
+   {
+      $oi->errln('Please specify changes flags.');
+      return;
+   }
+
+   $oi->writeln();
+
+   foreach($pages as $pagename)
+   {
+      $page = $collection->getPageAuto($pagename);
+      if($page instanceof \AnySrc\MarkdownBlog\Page)
       {
-         $pi = $mgr->getPluginByName($plugin);
-         if($pi instanceof \AnySrc\MarkdownBlog\BackendPluginBase)
+         if($enabled!==null)
          {
-            $hitems = $pi->getHelp();
-            if(count($hitems)>0)
-            {
-               $commands[] = array(
-                   "title" => $pi->getDisplayName(),
-                   "commands" => $hitems,
-               );
-            }
+            $page->setIsDisabled(($enabled===false));
          }
+         if($visible!==null)
+         {
+            $page->setIsVisible(($visible===true));
+         }
+         $page->write();
+         $oi->pageln($page);
       }
-   }
-
-   // Print help
-   foreach($commands as $section)
-   {
-      Stdout::nl();
-      Stdout::writel("[head]".$section['title']."[/head]");
-      $maxlength = max(array_map('strlen', array_keys($section['commands'])))+2;
-      foreach($section['commands'] as $cmd => $text)
+      else
       {
-         Stdout::write("[highlight]".str_pad($cmd, $maxlength, " ")."[/highlight] ");
-         Stdout::writel($text);
+         $oi->err('Page not found: ');
+         $oi->writeln($pagename);
       }
    }
 
-   return "";
-});
+   $oi->writeln();
+   $oi->writeln('Done!');
+})
+->setDescription('Change page states')
+->addArgument('pages', InputArgument::IS_ARRAY, 'List of filenames or hashes')
+->addOption('enable', null, InputOption::VALUE_NONE, 'Enable the pages')
+->addOption('disable', null, InputOption::VALUE_NONE, 'Disable the pages')
+->addOption('hide', null, InputOption::VALUE_NONE, 'Hide pages in lists')
+->addOption('show', null, InputOption::VALUE_NONE, 'Show pages in lists');
+
+
+//--> Upload URL
+$app->register('upload')->setCode(function(II $ii, OI $oi) use($cfg)
+{
+   $oi->writeln("Upload URL: <ch>".$cfg->getPath('hostconfig/cmdhost', '/')."session/upload</ch>");
+})
+->setDescription('Print the upload URL');
 
 
 //--> Load plugins
@@ -487,8 +380,5 @@ if($cfg->getPath('pluginsystem/enabled', false)===true)
 }
 
 
-//--> Launch silex with cmd args
-$argstr = "/".$args->get_arg_range(0, null, true, "/", false, true);
-
-$app->run(Request::create($argstr));
-Stdout::nl();
+//--> Run application
+$app->run(null, new OI());
